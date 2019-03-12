190a29d06f4e79d2df4cb513944ac34bd133caa0
Sergei Petrunia <psergey@askmonty.org>
2019-03-12 11:52:17 +0300

    Make Lock Tree's lock waits use externally provided mutexes/conditions
    In MyRocks, this allows:
    - SHOW PROCESSLIST to show "waiting for row lock" for waiting threads
    - KILL statement to abort the lock wait in the same way as it does it with
      point locking (without use of handlerton::kill_connection() method)
    
    - Remove previously added TransactionDB::KillLockWait() method as it is
      no longer needed.

--- utilities/transactions/pessimistic_transaction_db.cc
+++ utilities/transactions/pessimistic_transaction_db.cc
@@ -41,7 +41,11 @@ PessimisticTransactionDB::PessimisticTransactionDB(
                     ? txn_db_options_.custom_mutex_factory
                     : std::shared_ptr<TransactionDBMutexFactory>(
                           new TransactionDBMutexFactoryImpl())),
-      range_lock_mgr_(this) {
+      range_lock_mgr_(this,
+                      txn_db_options_.custom_mutex_factory?
+                         txn_db_options_.custom_mutex_factory :
+                           std::shared_ptr<TransactionDBMutexFactory>(
+                              new TransactionDBMutexFactoryImpl())) {
   assert(db_impl_ != nullptr);
   info_log_ = db_impl_->GetDBOptions().info_log;
 }
@@ -73,7 +77,11 @@ PessimisticTransactionDB::PessimisticTransactionDB(
                     ? txn_db_options_.custom_mutex_factory
                     : std::shared_ptr<TransactionDBMutexFactory>(
                           new TransactionDBMutexFactoryImpl())),
-      range_lock_mgr_(this) {
+      range_lock_mgr_(this,
+                      txn_db_options_.custom_mutex_factory
+                        ?  txn_db_options_.custom_mutex_factory
+                        : std::shared_ptr<TransactionDBMutexFactory>(
+                              new TransactionDBMutexFactoryImpl())) {
   assert(db_impl_ != nullptr);
 }
 
