c91095e08fbdacb69831a93ab403a80487dee435
Sergei Petrunia <psergey@askmonty.org>
2019-06-10 23:11:27 +0300

    Range Locking: address review input

--- utilities/transactions/transaction_lock_mgr.h
+++ utilities/transactions/transaction_lock_mgr.h
@@ -243,6 +243,9 @@ class RangeLockMgr :
 
   LockStatusData GetLockStatusData() override;
 
+  BaseLockMgr* GetManager() override {
+    return this;
+  }
  private:
   toku::locktree_manager ltm_;
 
